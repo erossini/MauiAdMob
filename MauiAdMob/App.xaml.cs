31a61e75cf6262fbc4c06ce7c573d2ed5140fda6
Enrico Rossini <enrico.rossini.uk@live.com>
2023-07-03 22:32:37 +0100

    Add basic project

---
+++ MauiAdMob/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Plugin.MauiMTAdmob;
+
+namespace MauiAdMob;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        CrossMauiMTAdmob.Current.UserPersonalizedAds = true;
+        CrossMauiMTAdmob.Current.ComplyWithFamilyPolicies = true;
+        CrossMauiMTAdmob.Current.UseRestrictedDataProcessing = true;
+        CrossMauiMTAdmob.Current.AdsId = DeviceInfo.Platform == DevicePlatform.Android ? "ca-app-pub-4381168884554284~9773728453" : "ca-app-pub-4381168884554284~4366323256";
+        CrossMauiMTAdmob.Current.TestDevices = new List<string>() { };
+
+        MainPage = new AppShell();
+	}
+}
